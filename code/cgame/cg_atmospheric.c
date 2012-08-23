9bfd0adc82c218f2b965fd9c72e9dc6bf23a11fa
Zack Middleton <zturtleman@gmail.com>
2012-08-23 04:48:33 +0000

    Fixed two 'variable frac set but not used' warnings.

--- code/cgame/cg_atmospheric.c
+++ code/cgame/cg_atmospheric.c
@@ -361,7 +361,7 @@ static void CG_RainParticleRender( cg_atmosphericParticle_t *particle ) {
 	vec3_t forward, right;
 	polyVert_t verts[3];
 	vec2_t line;
-	float len, frac, dist;
+	float len, dist;
 	vec3_t start, finish;
 	float groundHeight;
 //	int			msec = trap_Milliseconds();
@@ -387,7 +387,6 @@ static void CG_RainParticleRender( cg_atmosphericParticle_t *particle ) {
 	if ( start[2] <= groundHeight ) {
 		// Stop snow going through surfaces.
 		len = particle->height - groundHeight + start[2];
-		frac = start[2];
 		VectorMA( start, len - particle->height, particle->deltaNormalized, start );
 	}
 
@@ -557,7 +556,7 @@ static void CG_SnowParticleRender( cg_atmosphericParticle_t *particle ) {
 	vec3_t forward, right;
 	polyVert_t verts[3];
 	vec2_t line;
-	float len, frac, sinTumbling, cosTumbling, particleWidth, dist;
+	float len, sinTumbling, cosTumbling, particleWidth, dist;
 	vec3_t start, finish;
 	float groundHeight;
 //	int			msec = trap_Milliseconds();
@@ -586,7 +585,6 @@ static void CG_SnowParticleRender( cg_atmosphericParticle_t *particle ) {
 	if ( start[2] <= groundHeight ) {
 		// Stop snow going through surfaces.
 		len = particle->height - groundHeight + start[2];
-		frac = start[2];
 		VectorMA( start, len - particle->height, particle->deltaNormalized, start );
 	}
 
