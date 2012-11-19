38b594eff015fed36a43debe6c8b576bf6838109
Zack Middleton <zturtleman@gmail.com>
2012-11-19 06:08:49 +0000

    ioquake3 resync to revision 2362 from 2352.
    Fix some "> MAX_*" to be ">= MAX_*".
    Change error message in CL_ConfigstringModified to specify out of range index like in server.
    Fix follow command to find clients whose name begins with a number.
    - Fix up "gc" command, make it more like "tell". Based on patch by Ensiform.
    - Add usage messages for gc, tell, vtell, and votell commands.
    - Check player names in gc, tell, vtell, and votell commands.
    Remove anti tamper leftover code 'CL_ChangeReliableCommand'. From Ensiform.
    #5799 - Change messagemode text box to display colors like in console input box.
    Improve "play" command, based on a patch from Ensiform.
    Remove unneeded name buffer in S_Play_f.
    - Check for invalid filename in OpenAL's RegisterSound function.
    - Changed Base sound system to warn not error when sound filename is empty or too long.
    Remove references to non-existent functions CM_MarkFragments and CM_LerpTag.

--- code/game/g_cmds.c
+++ code/game/g_cmds.c
@@ -161,6 +161,32 @@ char	*ConcatArgs( int start ) {
 	return line;
 }
 
+
+/*
+==================
+StringIsInteger
+==================
+*/
+qboolean StringIsInteger( const char * s ) {
+	int			i;
+	int			len;
+	qboolean	foundDigit;
+
+	len = strlen( s );
+	foundDigit = qfalse;
+
+	for ( i=0 ; i < len ; i++ ) {
+		if ( !isdigit( s[i] ) ) {
+			return qfalse;
+		}
+
+		foundDigit = qtrue;
+	}
+
+	return foundDigit;
+}
+
+
 /*
 ==================
 ClientNumberFromString
@@ -175,24 +201,15 @@ int ClientNumberFromString( gentity_t *to, char *s, qboolean checkNums, qboolean
 	char		cleanName[MAX_STRING_CHARS];
 
 	if ( checkNums ) {
-		// numeric values are just slot numbers
-		if (s[0] >= '0' && s[0] <= '9') {
+		// numeric values could be slot numbers
+		if ( StringIsInteger( s ) ) {
 			idnum = atoi( s );
-			if ( idnum < 0 || idnum >= level.maxclients ) {
-				trap_SendServerCommand( to-g_entities, va("print \"Bad client slot: %i\n\"", idnum));
-				return -1;
+			if ( idnum >= 0 && idnum < level.maxclients ) {
+				cl = &level.clients[idnum];
+				if ( cl->pers.connected == CON_CONNECTED ) {
+					return idnum;
+				}
 			}
-
-			cl = &level.clients[idnum];
-			if ( cl->pers.connected != CON_CONNECTED ) {
-				trap_SendServerCommand( to-g_entities, va("print \"Client %i is not active\n\"", idnum));
-				return -1;
-			}
-			return idnum;
-		}
-
-		if ( !checkNames ) {
-			trap_SendServerCommand( to-g_entities, va("print \"%s is not a client number on the server\n\"", s));
 		}
 	}
 
@@ -208,9 +225,9 @@ int ClientNumberFromString( gentity_t *to, char *s, qboolean checkNums, qboolean
 				return idnum;
 			}
 		}
-
-		trap_SendServerCommand( to-g_entities, va("print \"User %s is not on the server\n\"", s));
 	}
+
+	trap_SendServerCommand( to-g_entities, va("print \"User %s is not on the server\n\"", s));
 
 	return -1;
 }
@@ -921,7 +938,7 @@ void G_Say( gentity_t *ent, gentity_t *target, int mode, const char *chatText ) 
 		color = COLOR_CYAN;
 		break;
 	case SAY_TELL:
-		if (target && g_gametype.integer >= GT_TEAM &&
+		if (target && target->inuse && target->client && g_gametype.integer >= GT_TEAM &&
 			target->client->sess.sessionTeam == ent->client->sess.sessionTeam &&
 			Team_GetLocationMsg(ent, location, sizeof(location)))
 			Com_sprintf (name, sizeof(name), EC"[%s%c%c"EC"] (%s)"EC": ", ent->client->pers.netname, Q_COLOR_ESCAPE, COLOR_WHITE, location );
@@ -992,13 +1009,14 @@ static void Cmd_Tell_f( gentity_t *ent ) {
 	char		*p;
 	char		arg[MAX_TOKEN_CHARS];
 
-	if ( trap_Argc () < 2 ) {
+	if ( trap_Argc () < 3 ) {
+		trap_SendServerCommand( ent-g_entities, "print \"Usage: tell <player id> <message>\n\"" );
 		return;
 	}
 
 	trap_Argv( 1, arg, sizeof( arg ) );
-	targetNum = atoi( arg );
-	if ( targetNum < 0 || targetNum >= level.maxclients ) {
+	targetNum = ClientNumberFromString( ent, arg, qtrue, qtrue );
+	if ( targetNum == -1 ) {
 		return;
 	}
 
@@ -1117,13 +1135,14 @@ static void Cmd_VoiceTell_f( gentity_t *ent, qboolean voiceonly ) {
 	char		*id;
 	char		arg[MAX_TOKEN_CHARS];
 
-	if ( trap_Argc () < 2 ) {
+	if ( trap_Argc () < 3 ) {
+		trap_SendServerCommand( ent-g_entities, va( "print \"Usage: %s <player id> <voice id>\n\"", voiceonly ? "votell" : "vtell" ) );
 		return;
 	}
 
 	trap_Argv( 1, arg, sizeof( arg ) );
-	targetNum = atoi( arg );
-	if ( targetNum < 0 || targetNum >= level.maxclients ) {
+	targetNum = ClientNumberFromString( ent, arg, qtrue, qtrue );
+	if ( targetNum == -1 ) {
 		return;
 	}
 
@@ -1229,24 +1248,45 @@ static char	*gc_orders[] = {
 	"report"
 };
 
+static const int numgc_orders = ARRAY_LEN( gc_orders );
+
 void Cmd_GameCommand_f( gentity_t *ent ) {
-	int		player;
-	int		order;
-	char	str[MAX_TOKEN_CHARS];
+	int			targetNum;
+	gentity_t	*target;
+	int			order;
+	char		arg[MAX_TOKEN_CHARS];
 
-	trap_Argv( 1, str, sizeof( str ) );
-	player = atoi( str );
-	trap_Argv( 2, str, sizeof( str ) );
-	order = atoi( str );
-
-	if ( player < 0 || player >= MAX_CLIENTS ) {
+	if ( trap_Argc() != 3 ) {
+		trap_SendServerCommand( ent-g_entities, va( "print \"Usage: gc <player id> <order 0-%d>\n\"", numgc_orders - 1 ) );
 		return;
 	}
-	if ( order < 0 || order > ARRAY_LEN( gc_orders ) ) {
+
+	trap_Argv( 1, arg, sizeof( arg ) );
+	targetNum = ClientNumberFromString( ent, arg, qtrue, qtrue );
+	if ( targetNum == -1 ) {
 		return;
 	}
-	G_Say( ent, &g_entities[player], SAY_TELL, gc_orders[order] );
-	G_Say( ent, ent, SAY_TELL, gc_orders[order] );
+
+	target = &g_entities[targetNum];
+	if ( !target || !target->inuse || !target->client ) {
+		return;
+	}
+
+	trap_Argv( 2, arg, sizeof( arg ) );
+	order = atoi( arg );
+
+	if ( order < 0 || order >= numgc_orders ) {
+		trap_SendServerCommand( ent-g_entities, va("print \"Bad order: %i\n\"", order));
+		return;
+	}
+
+	G_LogPrintf( "tell: %s to %s: %s\n", ent->client->pers.netname, target->client->pers.netname, gc_orders[order] );
+	G_Say( ent, target, SAY_TELL, gc_orders[order] );
+	// don't tell to the player self if it was already directed to this player
+	// also don't send the chat back to a bot
+	if ( ent != target && !(ent->r.svFlags & SVF_BOT)) {
+		G_Say( ent, ent, SAY_TELL, gc_orders[order] );
+	}
 }
 
 /*
